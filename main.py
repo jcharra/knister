21d904a55e641161b82e685ab09baa2e0650a596
jcharra <johannes.charra@gmail.com>
2023-05-17 23:18:36 +0200

    make knister game a class, add layout and react to click events

--- main.py
+++ main.py
@@ -1,94 +1,88 @@
 import random
 
 
-def roll():
-    return random.randint(1, 6) + random.randint(1, 6)
+class Knister:
+    def __init__(self):
+        self.field = [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0]
+        ]
 
+    def roll(self):
+        return random.randint(1, 6) + random.randint(1, 6)
 
-def show(field):
-    for row in field:
-        print(" ".join(f"{n:3}" for n in row))
+    def show(self):
+        for row in self.field:
+            print(" ".join(f"{n:3}" for n in row))
 
-
-def eval_group(values):
-    if 0 in values:
-        return 0
-
-    counts = [values.count(n) for n in range(2, 13)]
-    if 5 in counts:
-        return 10
-    elif 4 in counts:
-        return 6
-    elif 3 in counts:
-        if 2 in counts:
-            # Full house
-            return 8
-        else:
-            # three
-            return 3
-    elif 2 in counts:
-        return 3 if counts.count(2) > 1 else 1
-    else:
-        nums = sorted(values)
-        if nums == list(range(nums[0], nums[0] + 5)):
-            if 7 in nums:
-                return 10
-            else:
-                return 12
-        else:
+    @staticmethod
+    def eval_group(values):
+        if 0 in values:
             return 0
 
+        counts = [values.count(n) for n in range(2, 13)]
+        if 5 in counts:
+            return 10
+        elif 4 in counts:
+            return 6
+        elif 3 in counts:
+            if 2 in counts:
+                # Full house
+                return 8
+            else:
+                # three
+                return 3
+        elif 2 in counts:
+            return 3 if counts.count(2) > 1 else 1
+        else:
+            nums = sorted(values)
+            if nums == list(range(nums[0], nums[0] + 5)):
+                if 7 in nums:
+                    return 10
+                else:
+                    return 12
+            else:
+                return 0
 
-def evaluate(field):
-    total = 0
-    for idx, row in enumerate(field):
-        val = eval_group(row)
-        # print(f"Row {idx} has eval {val}")
-        total += val
+    def evaluate(self):
+        total = 0
+        for idx, row in enumerate(self.field):
+            val = self.eval_group(row)
+            total += val
 
-    for i in range(5):
-        val = eval_group([row[i] for row in field])
-        # print(f"Col {i} has eval {val}")
-        total += val
+        for i in range(5):
+            val = self.eval_group([row[i] for row in self.field])
+            # print(f"Col {i} has eval {val}")
+            total += val
 
-    diag1 = eval_group([field[i][i] for i in range(5)])
-    diag2 = eval_group([field[i][4-i] for i in range(5)])
+        diag1 = self.eval_group([self.field[i][i] for i in range(5)])
+        diag2 = self.eval_group([self.field[i][4-i] for i in range(5)])
 
-    print("Diag 1:", diag1)
-    print("Diag 2:", diag2)
+        total += diag1 * 2
+        total += diag2 * 2
 
-    total += diag1 * 2
-    total += diag2 * 2
-
-    return total
-
-
-def create_field():
-    return [
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]
-    ]
+        return total
 
 
 if __name__ == "__main__":
-    field = create_field()
+    k = Knister()
     rolls = 0
     while rolls < 25:
-        num = roll()
+        num = k.roll()
 
-        show(field)
-        print("Eval:", evaluate(field))
+        k.show()
+        print("Eval:", k.evaluate())
         print("ROLL:", num)
 
         while True:
             inp = input("Where? ")
             try:
                 row, col = int(inp[0]), int(inp[1])
-                if field[row][col] == 0:
-                    field[row][col] = num
+                if k.field[row][col] == 0:
+                    k.field[row][col] = num
                     break
                 else:
                     print("Nope - occupied!")
@@ -98,5 +92,5 @@ if __name__ == "__main__":
         rolls += 1
 
     print("Endstand:")
-    show(field)
-    print("PUNKTE:", evaluate(field))
+    k.show()
+    print("PUNKTE:", k.evaluate())
--- ui.py
+++ ui.py
@@ -1,18 +1,56 @@
+import random
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+
+from main import Knister
+
+
+class KnisterGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(cols=5, rows=5, **kwargs)
+
+        self.current_number = None
+        self.knister = Knister()
+
+        for idx in range(25):
+            b = Button(text="", font_size='40sp')
+            b.bind(on_press=lambda btn, i=idx: self.set_num(btn, i))
+            self.add_widget(b)
+
+    def set_num(self, button, idx):
+        if button.text == "" and self.knister.field[idx // 5][idx % 5] == 0:
+            button.text = str(self.current_number)
+            self.knister.field[idx // 5][idx % 5] = self.current_number
+            self.knister.show()
+            self.current_number = self.knister.roll()
+
+    def start(self):
+        self.current_number = self.knister.roll()
+        print("Current:", self.current_number)
 
 
 class KnisterApp(App):
     def build(self):
-        layout = GridLayout(cols=5, rows=5)
+        layout = BoxLayout(orientation="vertical")
 
-        for i in range(1, 26):
-            b = Button(text=f"{i}")
-            layout.add_widget(b)
+        self.next_num = Label(text="0", font_size='40sp', size_hint=(1.0, 1.0))
+        layout.add_widget(self.next_num)
+
+        self.knister_grid = KnisterGrid(size_hint=(1.0, 5.0))
+        layout.add_widget(self.knister_grid)
+
+        start_button = Button(
+            text="Start", font_size='40sp', size_hint=(1.0, 1.0))
+        start_button.bind(on_press=self.start_game)
+        layout.add_widget(start_button)
 
         return layout
+
+    def start_game(self, instance):
+        self.knister_grid.start()
 
 
 if __name__ == '__main__':
